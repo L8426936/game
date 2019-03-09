c8ed14f5a97039d0231b3db84e0a42f94cfead0d
L <1278764240@qq.com>
2019-03-09 15:59:15 +0800

    修改MostLinkLinkTest.java

--- src/solved/most_link_link/MostLinkLinkTest.java
+++ src/solved/most_link_link/MostLinkLinkTest.java
@@ -4,19 +4,19 @@ public class MostLinkLinkTest {
 	
 	public static void main(String[] args) {
 		// ------------------------------------------ 测试使用 ----------------------------------------------------
-		char[][] status = {
-				{MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EXIST, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.BAN},
-				{MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY},
-				{MostLinkLinkUtil.BAN, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.BAN, MostLinkLinkUtil.EMPTY},
-				{MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.BAN, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY},
-				{MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.BAN, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY}
-		};
-		MostLinkLinkUtil.printStatus(status);
-		int count = 25, startRow = 0, startCol = 2;
-		NodeTree nodeTree = new NodeTree(status, count, startRow, startCol);
-		nodeTree.DFS();
+//		char[][] status = {
+//				{MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EXIST, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.BAN},
+//				{MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY},
+//				{MostLinkLinkUtil.BAN, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.BAN, MostLinkLinkUtil.EMPTY},
+//				{MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.BAN, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY},
+//				{MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.BAN, MostLinkLinkUtil.EMPTY, MostLinkLinkUtil.EMPTY}
+//		};
+//		MostLinkLinkUtil.printStatus(status);
+//		int count = 25, startRow = 0, startCol = 2;
+//		NodeTree nodeTree = new NodeTree(status, count, startRow, startCol);
+//		nodeTree.DFS();
 		// --------------------------------------------------------------------------------------------------------
-//		MostLinkLinkUtil.autoPlay();
+		MostLinkLinkUtil.autoPlay();
 	}
 	
 }
